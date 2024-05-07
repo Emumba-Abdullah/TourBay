59c42286043f4b77634ba8b3984a719eec1c2797
MABA1001 <mughalabdullah2905@gmail.com>
2024-05-07 14:59:31 +0500

    Added Authentication with Jwt"

--- backend/server.ts
+++ backend/server.ts
@@ -1,12 +1,17 @@
 import dotenv from "dotenv";
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
 import { connect_database } from "./utils/connect_db";
+import userRouter from "./routes/userRouter";
 
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
+app.use("/user", userRouter);
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  res.status(500).json({ message: err.message });
+});
 
 connect_database();
 
